c1001dfa430810b60826b7aa25893129753e0cf8
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-03-17 21:24:33 +0530

    Course Controller is Added

---
+++ WEB-API/Pluralsight/Kevin Dockx/Building_RESTful_API_ASP.NET_Core/CourseLibrary/CourseLibrary/Profiles/CoursesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.Models;
+
+namespace CourseLibrary.Profiles
+{
+    public class CoursesProfile :Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
