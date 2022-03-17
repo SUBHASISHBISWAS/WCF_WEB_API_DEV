0f1568b4c3b14b5baf8bb74e42d2bcd98a134dbe
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-03-17 19:53:22 +0530

    Get Support is Added with formatter

---
+++ WEB-API/Pluralsight/Kevin Dockx/Building_RESTful_API_ASP.NET_Core/CourseLibrary/CourseLibrary/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var authorsFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            
+            return Ok(authorsFromRepo);
+        }
+    }
+}
