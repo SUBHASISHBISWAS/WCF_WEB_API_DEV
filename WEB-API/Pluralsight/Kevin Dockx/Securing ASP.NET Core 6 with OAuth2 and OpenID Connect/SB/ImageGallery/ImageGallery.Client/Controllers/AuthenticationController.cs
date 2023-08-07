866aa23ade489a4c04877c475d55d3881476c164
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-08-07 12:18:49 +0530

    Demo - Logging out of Our Web Application and Logging out of the Identity Provider

---
+++ WEB-API/Pluralsight/Kevin Dockx/Securing ASP.NET Core 6 with OAuth2 and OpenID Connect/SB/ImageGallery/ImageGallery.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Authorize]
+        public async Task Logout()
+        {
+            await Microsoft.AspNetCore.Authentication.AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await Microsoft.AspNetCore.Authentication.AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
